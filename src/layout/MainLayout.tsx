69a6d2bd0596c39809618cb6a3c2d60140a986e4
haneen <haneenhariry8@gmail.com>
2025-04-08 18:45:15 +0300

    layout create and npm react router dom and tailwindcss

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function MainLayout() {
+  return (
+    <div className=" h-screen bg-mainLayout xl:p-[70px] lg:p-[50px]">
+      <div className=" h-full bg-mainDiv border border-[#314158] rounded-[3px]">
+      <Outlet/>
+      </div>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layout/MainLayout.tsx'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout/>,
+    children :[
+      {
+
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider  router={routes}/>
   </StrictMode>,
 )
