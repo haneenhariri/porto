ea0df6e4a8ea9d372691bb8198418e0d1a2f604a
haneen <haneenhariry8@gmail.com>
2025-04-09 15:20:49 +0300

    hero section

---
+++ src/components/Hero/InfoSide.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import { InfoSideProps } from "../../types/hero";
+
+export default function InfoSide({name , jop , githubLink} : InfoSideProps) {
+  return (
+    <div className="flex flex-col gap-18.5">
+        <div>
+            <h3 className=" text-label xl:text-lg lg:text-base leading-7 mb-1">Hi all. I am</h3>
+            <h1 className=" xl:text-6xl lg:text-5xl leading-16 text-link-white">{name}</h1>
+            <h2 className=" xl:text-3xl lg:text-2xl text-jop">{jop}</h2>
+        </div>
+        <div>
+            <p className=" text-sm text-label mb-2">// find my profile on Github:</p>
+            <div className=" flex items-center gap-2.5 xl:text-base text-sm">
+                <span className=" text-jop">const</span>
+                <span className=" text-teal">githubLink</span>
+                <span className=" text-link-white">=</span>
+                <Link  className=" underline text-git"  to={githubLink}>“{githubLink}”</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
