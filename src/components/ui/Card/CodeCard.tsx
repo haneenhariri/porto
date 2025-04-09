d170ed9261da159bfe753ad0a63305de5bdda69f
haneen <haneenhariry8@gmail.com>
2025-04-09 14:14:24 +0300

    code card ui and install highlight

---
+++ src/components/ui/Card/CodeCard.tsx
@@ -0,0 +1,21 @@
+import hljs from "highlight.js";
+import { useEffect, useRef } from "react";
+import 'highlight.js/styles/atom-one-dark.css'; 
+
+export default function CodeCard({ code }: { code: string }) {
+    const codeRef = useRef<HTMLElement>(null);
+    useEffect(() => {
+        if (codeRef.current) {
+          hljs.highlightElement(codeRef.current);
+        }
+      }, []);
+  return (
+    <div className=" p-4 w-max border  border-border rounded-2xl bg-mainLayout ">
+      <pre>
+        <code ref={codeRef} className="language-typescript !bg-mainLayout">
+          {code}
+        </code>
+      </pre>
+    </div>
+  )
+}
