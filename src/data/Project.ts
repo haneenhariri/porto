6e2a45ed6744075e2e56e52fae6938eb4a04a6b5
haneen <haneenhariry8@gmail.com>
2025-04-16 10:34:17 +0300

    update

---
+++ src/data/Project.ts
@@ -0,0 +1,32 @@
+import project1 from '../assets/project/Annotation 2025-04-10 202054.png'
+import react from '../assets/react.svg'
+export const  ProjectData =
+[
+    {
+        projectName : 'proj1',
+        tools : ['react' , 'ts' , 'bootstrap'],
+        iconTools : react,
+        projDescription: 'courses platform',
+        projectLink:'https://haneenhariri.github.io/Flower-store/',
+        img: project1,
+        bgColor:'#A3B3FF'
+    },
+    {
+        projectName : 'proj1',
+        tools : ['react' , 'ts' , 'bootstrap'],
+        iconTools : 'next',
+        projDescription: 'courses platform',
+        projectLink:'',
+        img:'',
+        bgColor:'#A3B3FF'
+    },
+    {
+        projectName : 'proj1',
+        tools : ['react' , 'ts' , 'bootstrap'],
+        iconTools : 'pure',
+        projDescription: 'courses platform',
+        projectLink:'',
+        img:'',
+        bgColor:'#A3B3FF'
+    },
+]
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,23 @@
+import ProjCard from "../components/ui/Card/ProjCard"
+import { ProjectData } from "../data/Project"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className=" grid grid-cols-4">
+        {ProjectData.map((element, index) => 
+        (
+          <div>
+          <h2> <span>Project {index+1}</span> // _{element.projectName}</h2>
+          <ProjCard key={index} 
+          title={element.projectName}
+          img={element.img}
+          projectLink={element.projectLink}
+          description={element.projDescription}
+          tools={element.iconTools}
+          bgColor={element.bgColor} />
+          </div>
+        ))}
+    </div>
   )
 }
 
